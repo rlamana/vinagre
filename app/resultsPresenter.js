d19cda6910a15a3862c9a4a001a266721f249420
Ramón Lamana <rlamana@gmail.com>
2013-03-31 13:29:11 +0200

    Startups list view

---
+++ app/resultsPresenter.js
@@ -0,0 +1,22 @@
+define([], function () {
+	var ResultsPresenter = function (driver) {
+		this.driver = driver;
+		driver.connect(this.slots, this);
+	};
+
+	ResultsPresenter.prototype.slots = {
+		'search': function (keywords) {
+			// search
+
+			var fakeData = [
+				{id: 0, name: 'Sunrise', description: 'Building a better calendar', url: 'http://www.sunrise.am/'},
+				{id: 1, name: 'Stellarkite', description: 'Stellarkite is a multidisciplinary group of scientists and engineers born to geekify the world', url: 'http://www.stellarkite.com'},
+				{id: 2, name: 'Lovely', description: 'Building a platform for apartment rentals - a $10bn market opportunity', url: 'http://livelovely.com/'}
+			];
+
+			this.driver.render(fakeData);
+		}
+	};
+
+	return ResultsPresenter;
+});
---
+++ app/tpl/result.tpl
@@ -0,0 +1,5 @@
+<li data-id="{{id}}">
+	<h1>{{name}}</h1>
+	<p class="description">{{description}}</p>
+	<a class="url" href="{{url}}">{{url}}</a>
+</li>
